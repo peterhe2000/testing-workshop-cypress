e52d48d13780167f8c964b5d46f79b3a788e450d
Peter He <Peter@netwealth.com.au>
2019-09-23 13:52:29 +1000

    Made 01-04 test passed.

--- cypress/integration/01-basic/spec.js
+++ cypress/integration/01-basic/spec.js
@@ -2,14 +2,14 @@
 // @ts-check
 it('loads', () => {
   // application should be running at port 3000
-  cy.visit('localhost:3000')
+  cy.visit('localhost:3000');
 
   // this assertion fails on purpose
   // can you fix it?
-  cy.contains('h1', 'Todos App')
+  cy.contains('[data-cy=app-title]', 'todos');
 
   // also good practice is to use data attributes specifically for testing
   // see https://on.cypress.io/best-practices#Selecting-Elements
   // which play well with "Selector Playground" tool
   // how would you do select this element?
-})
+});
--- cypress/integration/02-adding-items/spec.js
+++ cypress/integration/02-adding-items/spec.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
-it('loads', () => {
+beforeEach(() => {
   // application should be running at port 3000
-  cy.visit('localhost:3000')
-  cy.contains('h1', 'todos')
-})
+  cy.request('POST', '/reset', {
+    todos: []
+  });
+  cy.visit('localhost:3000');
+  cy.contains('h1', 'todos');
+});
 
 it('starts with zero items', () => {
   // check if the list is empty initially
@@ -11,7 +14,8 @@ it('starts with zero items', () => {
   //   in the list
   //   use cy.get(...) and it should have length of 0
   //   https://on.cypress.io/get
-})
+  cy.get('li.todo').should('have.length', 0);
+});
 
 it('adds two items', () => {
   // repeat twice
@@ -19,22 +23,47 @@ it('adds two items', () => {
   //    type text and "enter"
   //    assert that the new Todo item
   //    has been added added to the list
-})
+  cy.get('.new-todo').type('first item{enter}');
+  cy.contains('li.todo', 'first item').should('be.visible');
+  cy.get('.new-todo').type('second item{enter}');
+  cy.contains('li.todo', 'second item').should('be.visible');
+});
+
+/**
+ * Adds a todo item
+ * @param {string} text
+ */
+const addItem = text => {
+  cy.get('.new-todo').type(`${text}{enter}`);
+};
 
 it('can add many items', () => {
-  const N = 5
+  const N = 5;
   for (let k = 0; k < N; k += 1) {
     // add an item
     // probably want to have a reusable function to add an item!
+    addItem(`item ${k}`);
   }
   // check number of items
-})
+  cy.get('li.todo').should('have.length', 5);
+});
 
 it('can mark items as completed', () => {
   // add a few items
   // mark items as completed
   // select completed items and confirm their number
-})
+
+  addItem('simple');
+  cy.contains('li.todo', 'simple')
+    .should('exist')
+    .find('input[type="checkbox"]')
+    .check();
+  // have to force click because the button does not appear unless we hover
+  cy.contains('li.todo', 'simple')
+    .find('.destroy')
+    .click({ force: true });
+  cy.contains('li.todo', 'simple').should('not.exist');
+});
 
 // what a challenge?
 // test more UI at http://todomvc.com/examples/vue/
